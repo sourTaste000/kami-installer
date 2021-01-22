d4ffe204976eb7432550715a602f0fd110f929b8
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2021-01-22 10:19:22 -0800

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::env;
+use std::fs;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const OS: &'static str = env::consts::OS;
@@ -13,43 +14,26 @@ struct Launcher {
 fn main() {
     println!("Welcome to the kami-installer v{}.", VERSION);
     let mut launcher_path: Vec<Launcher> = Vec::new();
+    let mut profiles: Vec<String> = Vec::new();
 
-    if OS == "macos" {
-        println!("macOS detected");
-        if Path::new("/Applications/MultiMC.app").exists() {
-            launcher_path.push(Launcher {
-                form: "multimc".to_string(),
-                path: "/Applications/MultiMC.app".to_string()
-            })
+    match OS {
+        "macos" => {
+            if Path::new("/Applications/MultiMC.app").exists() {
+                let files = fs::read_dir("/Applications/MultiMC.app/Contents/MacOS/instances").unwrap();
+                for entry in files {
+                    let entry = entry.unwrap();
+                    profiles.push(format!("{:?}", entry.path().file_name().ok_or("No profiles found!")));
+                }
+            }
+            if Path::new("/Applications/Minecraft.app").exists() {
+                //
+            }
         }
-
-        if Path::new("/Applications/Minecraft.app").exists() {
-            launcher_path.push( Launcher {
-                form: "vanilla".to_string(),
-                path: "/Applications/Minecraft.app".to_string()
-            })
-        }
-    } else if OS == "linux" {
-        println!("linux detected");
-    } else if OS == "windows" {
-        println!("windows detected");
-        if Path::new("C:\\Program Files\\MultiMC").exists() {
-            launcher_path.push(Launcher {
-                form: "multimc".to_string(),
-                path: "C:\\Program Files\\MultiMC".to_string()
-            })
-        }
-
-        if Path::new("C:\\Program Files\\Minecraft").exists() {
-            launcher_path.push(Launcher {
-                form: "vanilla".to_string(),
-                path: "C:\\Program Files\\Minecraft".to_string()
-            })
-        }
+        _ => panic!("Your operating system {:?} is not supported right now.", OS)
     }
 
-    println!("Found {} instances.", launcher_path.len());
-    for i in launcher_path {
-        print!("Launcher type: {}, Launcher path: {}\n", i.form, i.path);
+    println!("Found {} profiles.", profiles.len());
+    for i in profiles {
+        println!("{}", i);
     }
 }
