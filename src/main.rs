8db9793f3b044bb5fea6c2039cb262b31bd8b0cd
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2021-01-22 11:30:49 -0800

    filtering

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,22 @@ struct Launcher {
 
 fn main() {
     println!("Welcome to the kami-installer v{}.", VERSION);
-    let mut launcher_path: Vec<Launcher> = Vec::new();
     let mut profiles: Vec<String> = Vec::new();
+    let mut sanitized_profiles: Vec<String> = Vec::new();
 
     match OS {
         "macos" => {
             if Path::new("/Applications/MultiMC.app/Contents/MacOS/instances").exists() {
                 for entry in fs::read_dir("/Applications/MultiMC.app/Contents/MacOS/instances").unwrap() {
                     let entry = entry.unwrap();
-                    profiles.push(format!("{:?}", entry.path().file_name().ok_or("No profiles found!")));
+                    profiles.push(format!("{:?}", entry.path().file_name().ok_or("Not found!")));
+                }
+                for instances in &profiles {
+                    let sanitized = instances.replace("Ok(\"", "").replace("\")", "");
+                    if sanitized == "instgroups.json" || sanitized == "_MMC_TEMP"{
+                        continue
+                    }
+                    sanitized_profiles.push(sanitized)
                 }
             }
             // if Path::new("/Applications/Minecraft.app").exists() {
@@ -41,8 +48,8 @@ fn main() {
         _ => panic!("Your operating system {:?} is not supported right now.", OS)
     }
 
-    println!("Found {} profiles.", profiles.len());
-    for versions in profiles {
+    println!("Found {} profiles.", sanitized_profiles.len());
+    for versions in sanitized_profiles {
         println!("{}", versions);
     }
 }
