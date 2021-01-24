3031381285bc86dba7948cee855ec71055ae5148
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2021-01-24 13:15:02 -0800

    very based

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 use std::path::Path;
 use std::env;
 use std::fs;
+use download_rs::sync_download::Download;
+use std::error::Error;
+use std::process::Command;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const OS: &'static str = env::consts::OS;
-
-
-struct Launcher {
-    form: String,
-    path: String
-}
 
 fn main() {
     println!("Welcome to the kami-installer v{}.", VERSION);
@@ -49,7 +46,47 @@ fn main() {
     }
 
     println!("Found {} profiles.", sanitized_profiles.len());
-    for versions in sanitized_profiles {
-        println!("{}", versions);
+    for i in 0..sanitized_profiles.len() {
+        println!("{}. {}", i, sanitized_profiles[i])
+    }
+
+    let mut selected = String::new();
+    println!("Choose the profile you want to install: ");
+    let _ = std::io::stdin().read_line(&mut selected).unwrap();
+    let chosen_profile = sanitized_profiles[selected.trim().parse::<usize>().unwrap()].to_owned();
+
+    let version = match fs::read_to_string(format!("/Applications/MultiMC.app/Contents/MacOS/instances/{}/mmc-pack.json", chosen_profile)) {
+        Ok(contents) => contents,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+
+    // I got lazy here
+    if version.contains("1.12.2") {
+        println!("1.12.2")
+    } else if version.contains("1.16.4") {
+        let download = match OS {
+            "macos" => {
+                Command::new("curl")
+                    .arg("-LO")
+                    .arg("https://github.com/zeroeightysix/KAMI/releases/download/1.16.4-dec/KAMI-1.16.4-dec.jar")
+                    .output()
+                    .expect("Failed to execute!");
+            }
+            _ => panic!("not supported")
+        };
+        let mv = match OS {
+            "macos" => {
+                Command::new("mv")
+                    .arg("./KAMI-1.16.4-dec.jar")
+                    .arg(format!("/Applications/MultiMC.app/Contents/MacOS/instances/{}/.minecraft/mods/", chosen_profile))
+                    .output()
+                    .expect("Failed to execute")
+            }
+            _ => panic!("Not supported")
+        };
+        println!("done!")
+    } else {
+        panic!("Version not supported!")
     }
 }
